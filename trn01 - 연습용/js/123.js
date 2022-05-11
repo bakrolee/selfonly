ddb4e26795942d69f896fda69842f1bb9e7ad11d
bakrolee <104603760+bakrolee@users.noreply.github.com>
2022-05-11 14:55:53 +0900

    Add files via upload

---
+++ trn01 - 연습용/js/123.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.container').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+    })
+})
